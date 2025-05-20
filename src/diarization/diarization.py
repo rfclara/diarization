e08ad7171739374a59f90c3ecfbfc1584c02cc94
rfclara <c.rosinafernandez@gmail.com>
2025-05-20 14:10:39 +0200

    Refactor audio diarization function to include audio pre-loading following tuto

--- src/diarization/diarization.py
+++ src/diarization/diarization.py
@@ -3,23 +3,27 @@ from pyannote.audio import Pipeline
 from pathlib import Path
 import torch
 import textgrid
+import torchaudio
 
 """From the audio file, returns the diarization result in RTTM and TextGrid format
-conains the speaker id, start time, end time"""
+contains the speaker id, start time, end time"""
 
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1",
-                                            use_auth_token="YOUR_TOKEN")
+                                    use_auth_token="YUR_HUGGINGFACE_TOKEN",)
 
 pipeline.to(torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"))
 print("Using device:", pipeline.device)
 
-def diarize_audio(audio_file,num_speakers=None):
-    diarization = pipeline(audio_file, num_speakers=num_speakers)
+def diarize_audio(audio_file, num_speakers=None):
+    # Pre-load audio in memory
+    waveform, sample_rate = torchaudio.load(audio_file)
+    audio_input = {"waveform": waveform, "sample_rate": sample_rate}
+    diarization = pipeline(audio_input, num_speakers=num_speakers)
     audio_path = Path(audio_file)
     rttm_filename = audio_path.with_suffix('.rttm')
     textgrid_filename = audio_path.with_suffix('.TextGrid')
     with rttm_filename.open("w") as rttm:
-       diarization.write_rttm(rttm)
+        diarization.write_rttm(rttm)
     write_textgrid(diarization, textgrid_filename)
 
 def write_textgrid(diarization, textgrid_filename):
